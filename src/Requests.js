a036469633d35c71e2bc3b22c97c4abd00e5201b
represent81400 <marinaljesevic@gmail.com>
2023-09-11 13:04:32 +0200

    MovieTeka App

---
+++ src/Requests.js
@@ -0,0 +1,10 @@
+const key = '9c0b220910d331dbc4a96cebf9ce4f1a';
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+ function App() {
+  return (
+    <footer
+      className="bg-neutral-900 text-center text-red-600">
+      <div className="p-4 text-center text-2xl font-bold">
+      © 2023 <span> </span>  
+        <a
+          className="text-red-600 text-4xl font-bold cursor-pointer"
+          href="https://tailwind-elements.com/"
+        >MovieTeka</a>
+      </div>
+    </footer>
+  );
+}
+
+export default App
